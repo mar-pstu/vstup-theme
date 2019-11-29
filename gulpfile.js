6c17b8d4c43210287e9dc92b71ce60c18a1c1256
chomovva\admin <chomovva@gmail.com>
2019-11-29 17:13:54 +0200

    v0.0.1

---
+++ gulpfile.js
@@ -0,0 +1,180 @@
+'use strict';
+
+var gulp           = require( 'gulp' );
+var styles         = require( 'gulp-sass' );
+var html           = require( 'gulp-pug' );
+var plumber        = require( 'gulp-plumber' );
+var autoprefixer   = require( 'gulp-autoprefixer' );
+var images         = require( 'gulp-imagemin' );
+var sourcemaps     = require( 'gulp-sourcemaps' );
+var minscripts     = require( 'gulp-uglify' );       // минифицирует js
+var minstyles      = require( 'gulp-clean-css' );
+var rename         = require( 'gulp-rename' );       // добавляет суфикс min к имени файла
+var cache          = require( 'gulp-cache' );
+var del            = require( 'del' );
+var browserSync    = require( 'browser-sync' );
+var zip            = require( 'gulp-zip' );
+
+
+
+styles.compiler      = require( 'node-sass' );
+
+
+
+
+gulp.task( 'packtheme', function() {
+	return gulp.src( [ './**/**/*', '!./node_modules', '!./src', '!./gulpfile.js', '!./package-lock.json', '!./package.json' ] )
+		.pipe( zip( 'dumdj-theme.zip' ) )
+		.pipe( gulp.dest( '../') );
+	}
+);
+
+
+
+gulp.task( 'packsrc', function() {
+	return gulp.src( './src/*' )
+		.pipe( zip( 'dumdj-src.zip' ) )
+		.pipe( gulp.dest( '../') );
+	}
+);
+
+
+
+gulp.task( 'packproject', function() {
+	return gulp.src( [ './*', '!./node_modules', '!./images', '!./styles', '!./scripts', '!./fonts', '!./video', '!./examples' ] )
+		.pipe( zip( 'dumdj-project.zip' ) )
+		.pipe( gulp.dest( '../') );
+	}
+);
+
+
+
+gulp.task( 'video', function () {
+	return gulp.src( './src/video/*.*' )
+		.pipe( gulp.dest( './video/' ) );
+} );
+
+
+
+gulp.task( 'fonts', function () {
+	return gulp.src( './src/fonts/*.*' )
+		.pipe( gulp.dest( './fonts/' ) );
+} );
+
+
+ 
+gulp.task( 'styles', function () {
+	return gulp.src( './src/styles/*.scss' )
+		.pipe( plumber() )
+		.pipe( sourcemaps.init() )
+		.pipe( styles().on( 'error', styles.logError ) )
+		.pipe( autoprefixer() )
+		.pipe( sourcemaps.write( '.' ) )
+		.pipe( gulp.dest( './styles/' ) )
+		.on( 'end', browserSync.reload );
+} );
+
+
+gulp.task( 'minstyles', function () {
+	return gulp.src( [ './styles/*.css', '!./styles/*.min.css' ] )
+		.pipe( minstyles( { compatibility: 'ie8' } ) )
+		.pipe( rename( { suffix: '.min' } ) )
+		.pipe( gulp.dest( './styles/' ) );
+} );
+ 
+
+
+gulp.task( 'html', function () {
+	return gulp.src( [ './src/views/*.pug', '!./src/views/index.pug' ] )
+		.pipe( plumber() )
+		.pipe( html( { pretty: true } ) )
+		.pipe( gulp.dest( './examples/' ) )
+		.on( 'end', browserSync.reload );
+} );
+
+
+
+gulp.task( 'index', function () {
+	return gulp.src( './src/views/index.pug' )
+		.pipe( plumber() )
+		.pipe( html( { pretty: true } ) )
+		.pipe( gulp.dest( './' ) )
+		.on( 'end', browserSync.reload );
+} );
+
+
+
+gulp.task( 'scripts', function () {
+	return gulp.src( './src/scripts/*.js' )
+		.pipe( plumber() )
+		.pipe( gulp.dest( './scripts/' ) )
+		.pipe( minscripts() )
+		.pipe( rename( { suffix: '.min' } ) )
+		.pipe( gulp.dest( './scripts/' ) )
+		.on( 'end', browserSync.reload );
+} );
+
+
+
+gulp.task( 'minscripts', function () {
+	return gulp.src( [ './scripts/*.js', '!./scripts/*.min.js' ] )
+		.pipe( plumber() )
+		.pipe( minscripts() )
+		.pipe( rename( { suffix: '.min' } ) )
+		.pipe( gulp.dest( './scripts/' ) );
+} );
+
+
+
+gulp.task( 'images', function () {
+		return gulp.src( './src/images/**/*.{png,jpg,svg,gif}' )
+				.pipe( plumber() )
+				.pipe( cache( images() ) )
+				.pipe( gulp.dest( './images/' ) )
+				.on( 'end', browserSync.reload );
+		}
+);
+
+
+
+gulp.task( 'server', function () {
+	browserSync.init({
+		server: {
+			baseDir: './'
+		}
+	} );
+} );
+
+
+gulp.task( 'removebuild', function () {
+	return del.sync( '/examples/' );
+} );
+
+
+
+
+gulp.task( 'clearcache', function () {
+	return cache.clearAll();
+} );
+
+
+
+gulp.task( 'minify', gulp.series( 'minstyles', 'minscripts' ) );
+
+
+
+gulp.task( 'watch', function () {
+	gulp.watch( './src/styles/**/*.scss',                gulp.series( 'styles') );
+	gulp.watch( './src/views/**/*.pug',                  gulp.series( 'html', 'index' ) );
+	gulp.watch( './src/scripts/**/*.js',                 gulp.series( 'scripts' ) );
+	gulp.watch( './src/images/**/*.{png,jpg,svg,gif}',   gulp.series( 'images' ) );
+	gulp.watch( './src/video/**/*.*',                    gulp.series( 'video' ) );
+	gulp.watch( './src/fonts/**/*.*',                    gulp.series( 'fonts' ) );
+} );
+
+
+
+gulp.task( 'default', gulp.series(
+	gulp.parallel( 'html', 'index', 'styles', 'scripts', 'images', 'video', 'fonts' ),
+	gulp.parallel( 'watch', 'server' )
+) );
---
+++ scripts/main.js
@@ -0,0 +1,190 @@
+jQuery.each( [ 'pdf', 'doc', 'xls', 'zip', 'ppt', 'odt', 'psd' ], function( i, type ) {
+	var selector;
+	switch( type ) {
+		case 'doc':
+			selector = 'a[href$=".doc"], a[href$=".docx"], a[href$=".docm"], a[href$=".rtf"]';
+			break;
+		case 'xls':
+			selector = 'a[href$=".xls"], a[href$=".xlsx"], a[href$=".ods"], a[href$=".csv"], a[href$=".xlsm"]';
+			break;
+		case 'zip':
+			selector = 'a[href$=".zip"], a[href$=".rar"], a[href$=".7z"]';
+			break;
+		case 'ppt':
+			selector = 'a[href$=".ppt"], a[href$=".pptx"], a[href$=".odp"], a[href$=".pptm"]';
+			break;
+		default:
+			selector = 'a[href$=".' + type + '"]';
+			break;
+	}
+	jQuery( selector ).each( function ( index, element ) {
+		var $link = jQuery( element );
+		if (
+			$link.find( '.file, .file-' + type ).length < 1 &&
+			! $link.hasClass( 'no-file-icon' ) &&
+			! $link.hasClass( 'button' ) &&
+			! $link.hasClass( 'btn' ) &&
+			! $link.hasClass( 'wp-block-file__button' ) &&
+			$link.children( 'img' ).length < 1
+		) {
+			$link.prepend( jQuery( '<span>', {
+				class: 'file file-' + type
+			} ) );
+		}
+	} );
+} );
+
+
+
+
+
+
+
+
+
+
+/* Навигационное меню */
+jQuery( document ).ready( function () {
+
+	jQuery( 'body' ).on( 'click', '.burger', function() {
+		if ( jQuery( 'body' ).attr( 'data-nav' ) == 'active' ) {
+			jQuery( '#nav' ).removeClass( 'nav--active' );
+			jQuery( '.burger' ).removeClass( 'burger--active' );
+			jQuery( 'body' ).attr( 'data-nav', 'inactive' ).css( { 'overflow': 'auto' } );
+		} else {
+			jQuery( '#nav' ).addClass( 'nav--active' );
+			jQuery( '.burger' ).addClass( 'burger--active' );
+			jQuery( 'body' ).attr( 'data-nav', 'active' ).css( { 'overflow': 'hidden' } );
+		}
+	} )
+
+} );
+
+
+
+
+
+
+
+
+
+
+/* кнопка наверх */
+jQuery( document ).ready( function () {
+  var $w = jQuery( window ),
+    $toTopBtn = jQuery( '<button>', {
+      class: 'to-top-btn',
+      id: 'toTopBtn',
+      title: studyinukraineTheme.toTopBtn,
+    } ).appendTo( jQuery( 'body' ) );
+  function _btnHide() {
+    if ( $w.scrollTop() > screen.height * 0.5) {
+      $toTopBtn.show();
+    } else {
+      $toTopBtn.hide();
+    }
+  }
+  function _toTopBtnClick() {
+    jQuery( 'body, html' ).animate( {
+      scrollTop: 0
+    }, 500 );
+    return false;
+  }
+  _btnHide();
+  $w.bind( 'scroll', _btnHide );
+  $toTopBtn.bind( 'click', _toTopBtnClick );
+} );
+
+
+
+
+
+
+
+// jQuery( document ).ready( function () {
+
+
+// 	jQuery( '.card [data-card="toggle"]' ).on( 'click', function ( event ) {
+// 		var $entry = jQuery( this ).closest( '.card' ),
+// 			class_name = 'card--active';
+// 		event.preventDefault();
+// 		$entry.toggleClass( class_name );
+// 	} )
+
+
+// 	jQuery( '#search-button' ).fancybox();
+// 	jQuery( '.fancybox' ).fancybox();
+
+// } );
+
+
+
+
+
+
+
+( function () {
+
+
+	/**
+	*	"Аккордеоны"
+	*/
+
+		
+	function toggle( e ) {
+		e.preventDefault();
+		var $heading = jQuery( this ),
+			$section = $heading.parent( '.accordio__listitem' ),
+			$content = $heading.siblings( '.listitem__content' );
+		if ( $section.hasClass( 'active' ) ) {
+			$section.removeClass( 'active' );
+			$content.slideUp();
+		} else {
+			$section.addClass( 'active' );
+			$content.slideDown();
+		}
+
+	}
+
+	function add_section( el, image ) {
+		var $panel,
+			$content,
+			$section;
+		$section = jQuery( '<div>', { 'class': 'accordio__listitem listitem', 'role': 'listitem' } );
+		$panel = jQuery( '<div>', { 'class': 'listitem__panel panel', click: toggle } );
+		if ( image ) jQuery( '<div>', { 'class': 'thumbnail' } )
+			.append( image.attr( 'class', 'lazy' ) )
+			.appendTo( $panel );
+		el.attr( 'class', 'title' ).appendTo( $panel );
+		$content = jQuery( '<div>', { 'class': 'listitem__content content' } ).css( 'display', 'none' );
+		$section.append( $panel );
+		$section.append( $content );
+		return $section;
+	}
+
+
+	function build( index, accordio ) {
+		var $accordio = jQuery( accordio ),
+			selector = $accordio.attr( 'data-heading' ),
+			$section;
+		$accordio.children().each( function( i, el ) {
+			var  $el = jQuery( el );
+			if ( $el.is( selector ) ) {
+				$section = add_section( $el, false );
+				$accordio.append( $section );
+			} else {
+				if ( $el.is( '.heading' ) && $el.find( selector ).length > 0 ) {
+					$section = add_section( $el.find( '.title' ), $el.find( 'img.heading__thumbnail' ) );
+					$el.remove();
+					$accordio.append( $section );
+				} else {
+					if ( $section != undefined ) $el.appendTo( $section.children( '.content' ).eq( 0 ) );
+				}
+			}
+		} );
+	}
+
+	jQuery( '.accordio[ data-heading ]' ).each( build );
+
+
+} )();
