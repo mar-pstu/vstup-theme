850bd21facb71eb78862fb69fe6f56f31ba3c31e
chomovva\admin <chomovva@gmail.com>
2020-08-06 15:11:50 +0300

    переделал мобильное меню и немного стили блока "О нас"

--- src/scripts/main/nav.js
+++ src/scripts/main/nav.js
@@ -2,8 +2,11 @@
 jQuery( document ).ready( function () {
 
 	var $window = jQuery( window );
-		$nav_list = jQuery( '.wrapper__item--header .nav .nav__list' ).eq( 0 );
-		$wpadminbar = jQuery( '#wpadminbar' );
+	var $nav_list = jQuery( '.wrapper__item--header .nav .nav__list' ).eq( 0 );
+	var $wpadminbar = jQuery( '#wpadminbar' );
+	var $mobile_list_container = $nav_list.clone().attr( 'class', 'list-container' );
+	var $mobile_parent_items = $mobile_list_container.find( '> .has-sub-menu' );
+	var $mobile_child_menus = $mobile_parent_items.find( '> .sub-menu' );
 
 	function wpadminbar_offset_top() {
 		jQuery( '.header.header--fixed' ).css( 'top', $wpadminbar.height() );
@@ -22,7 +25,21 @@ jQuery( document ).ready( function () {
 	} );
 
 
-	$nav_list.clone().removeClass().appendTo( jQuery( '#mobilenav-list' ) );
+	$mobile_list_container.appendTo( jQuery( '#mobilenav-list' ) );
+	$mobile_child_menus.hide( 0 );
+
+	$mobile_parent_items.each( function( index, element ) {
+		var $element = jQuery( element );
+		var $link = $element.find( '> a' ).clone().removeClass();
+		if ( $link.attr( 'href' ).length > 1 ) {
+			$link.appendTo( jQuery( '<li>' ).prependTo( $element.find( '.sub-menu' ).eq( 0 ) ) );
+		}
+	} );
+
+	$mobile_parent_items.click( function ( event ) {
+		event.preventDefault();
+		jQuery( this ).find( '> .sub-menu' ).slideToggle( 200 );
+	} );
 
 	$nav_list.find( ' > .has-sub-menu' ).each( function ( index, element ) {
 		var $sub_menus = jQuery( element ).find( '.sub-menu' );
