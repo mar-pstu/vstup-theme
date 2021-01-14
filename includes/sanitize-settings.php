d1569ac4ae90efd84819d532390e52c8a3a743c7
chomovva\admin <chomovva@gmail.com>
2021-01-14 13:28:51 +0200

    отключил валидацию настроек новостей

--- includes/sanitize-settings.php
+++ includes/sanitize-settings.php
@@ -7,31 +7,31 @@ namespace vstup;
 if ( ! defined( 'ABSPATH' ) ) { exit; };
 
 
-/**
- * Очистка новостей (главная страница)
- * */
-function sanitize_news_entries_setting( $value, $old_value = [] ) {
-	$value = ( is_array( $value ) ) ? array_filter( array_map( function ( $entry ) {
-		return parse_only_allowed_args(
-			[
-				'title' => '',
-				'link'  => '',
-				'thumbnail' => '',
-			],
-			$entry,
-			[ 'sanitize_text_field', 'esc_url_raw', 'esc_url_raw' ],
-			[ 'title', 'link', 'thumbnail' ],
-			[ 'title', 'link', 'thumbnail' ]
-		);
-	}, $value ) ) : [];
-	$news_numberentries = get_theme_mod( 'news_numberentries' );
-	if ( count( $value ) > $news_numberentries ) {
-		$value = array_slice( $value, 0, $news_numberentries );
-	}
-	return $value;
-}
+// /**
+//  * Очистка новостей (главная страница)
+//  * */
+// function sanitize_news_entries_setting( $value, $old_value = [] ) {
+// 	$value = ( is_array( $value ) ) ? array_filter( array_map( function ( $entry ) {
+// 		return parse_only_allowed_args(
+// 			[
+// 				'title' => '',
+// 				'link'  => '',
+// 				'thumbnail' => '',
+// 			],
+// 			$entry,
+// 			[ 'sanitize_text_field', 'esc_url_raw', 'esc_url_raw' ],
+// 			[ 'title', 'link', 'thumbnail' ],
+// 			[ 'title', 'link', 'thumbnail' ]
+// 		);
+// 	}, $value ) ) : [];
+// 	$news_numberentries = get_theme_mod( 'news_numberentries' );
+// 	if ( count( $value ) > $news_numberentries ) {
+// 		$value = array_slice( $value, 0, $news_numberentries );
+// 	}
+// 	return $value;
+// }
 
-add_filter( 'pre_set_theme_mod_' . 'news_entries', 'vstup\sanitize_news_entries_setting', 10, 2 );
+// add_filter( 'pre_set_theme_mod_' . 'news_entries', 'vstup\sanitize_news_entries_setting', 10, 2 );
 
 
 /**
